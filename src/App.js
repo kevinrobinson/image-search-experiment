f08ce5f5ee8d3815dc8eeab11d063a49a5beeb8e
kevinrobinson <kevin.robinson.0@gmail.com>
2019-02-21 11:53:19 -0500

    Experiment for youtube skinning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import qs from 'query-string';
 import './App.css';
 
-const KEY = 'AIzaSyCsnowZbjZ7Sap02rPMRZ05aWFcbvk1CeU';
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
       error: null,
-      json: null
+      json: null,
+      apiKey: null
     };
 
     this.onQueryChange = this.onQueryChange.bind(this);
@@ -17,14 +17,23 @@ class App extends Component {
     this.onFetchError = this.onFetchError.bind(this);
   }
 
+  componentDidMount() {
+    const queryString = qs.parse(window.location.search);
+    const apiKey = queryString.api_key;
+    this.setState({apiKey});
+  }
+
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) this.fetch();
+    if (this.state.apiKey && prevState.query !== this.state.query) this.fetch();
   }
 
   fetch() {
     const {query} = this.state;
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${KEY}`;
-    fetch(url)
+    const endpoint = 'https://services-edu.herokuapp.com/youtube/search';
+    const url = `${endpoint}?part=snippet&q=${query}`;
+
+    const headers = {'X-Services-Edu-Api-Key': 'abc'};
+    fetch(url, {headers})
       .then(response => response.json())
       .then(this.onFetchDone)
       .catch(this.onFetchError);
@@ -36,7 +45,6 @@ class App extends Component {
   }
 
   onFetchDone(json) {
-    console.log('json', json);
     this.setState({json});
   }
 
@@ -45,25 +53,33 @@ class App extends Component {
   }
 
   render() {
-    const {error, json, query} = this.state;
+    const {apiKey} = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
-          <div>
-            <div>query: <input onChange={this.onQueryChange} type="text" value={query} /></div>
-            <div>error: {error && JSON.stringify(error, null, 2)}</div>
-            <div>json: {json && this.renderJson(json)}</div>
-          </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {!apiKey ? this.renderMissingApiKey() : this.renderSearch()}
         </header>
+      </div>
+    );
+  }
+
+  renderMissingApiKey() {
+    return (
+      <div>
+        <div>You need an API key to get started.</div>
+        <div>When you get one, add it to the URL like `localhost:3000/videos?api_key=abc`</div>
+      </div>
+    );
+  }
+
+  renderSearch() {
+    const {error, json, query} = this.state;
+    return (
+      <div>
+        <div>search: <input onChange={this.onQueryChange} type="text" value={query} /></div>
+        {json && this.renderJson(json)}
+        {error && <div>error: {JSON.stringify(error, null, 2)}</div>}
       </div>
     );
   }
@@ -72,7 +88,7 @@ class App extends Component {
     return (
       <div>
         {json.items.map(item => (
-          <div>
+          <div key={JSON.stringify(item.id)}>
             <div>{item.snippet.title}</div>
             <div>{item.snippet.description}</div>
             <div><img src={item.snippet.thumbnails.default.url} alt="thumbnail" /></div>
@@ -82,5 +98,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
